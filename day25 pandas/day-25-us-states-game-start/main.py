35636d47b28a1d02e29cee3198010aad7ec77f16
Sudipto <153379945+Sudipto-git@users.noreply.github.com>
2025-01-21 03:19:47 +0530

    day 25 done

---
+++ day25 pandas/day-25-us-states-game-start/main.py
@@ -0,0 +1,67 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+image = "/Users/sudipto/Documents/code/projects/100days of code/day25 pandas/day-25-us-states-game-start/blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+                # to get the exact location on the map using this code
+                # def get_mouse_click_coor(x, y):
+                #     print(x, y)
+                # turtle.onscreenclick(get_mouse_click_coor)
+                # turtle.mainloop()
+
+data = pandas.read_csv("/Users/sudipto/Documents/code/projects/100days of code/day25 pandas/day-25-us-states-game-start/50_states.csv")
+all_states = data.state.to_list()
+gussed_states = []
+
+
+# taking the name of the states as inout in the map
+
+
+for state in all_states:
+    answer_state = screen.textinput(title = f"{len(gussed_states)}/50Guess the State", prompt="What's another state's name?").title()
+    print(answer_state)
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in gussed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        print("The game has ended")
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        
+        gussed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.speed(4)
+        t.penup()
+        state_data = data[data.state == answer_state] # this will give the data of the state that the user has entered
+        t.goto(float(state_data.x), float(state_data.y))
+        t.write(answer_state)
+        
+#states to learn.csv
+# if the user types exit, then the game should end and the states that they did not guess should be written in states to learn.csv
+
+
+    
+                    #if the answer state is in the csv file
+                    # if they got it right:
+                            # create a turtle that will move to the right state along with the name of the state
+
+
+
+
+
+screen.exitonclick()
+
+
+
--- day25 pandas/main.py
+++ day25 pandas/main.py
@@ -18,11 +18,53 @@
 
 import pandas
 
-data = pandas.read_csv("/Users/sudipto/Documents/code/projects/100days of code/day25 pandas/weather_data.csv") 
+# data = pandas.read_csv("/Users/sudipto/Documents/code/projects/100days of code/day25 pandas/weather_data.csv") 
 # print(data["temp"] )
 # print(type(data))
-temp_lis = data["temp"].to_list()
-print(temp_lis)
-avg = sum(temp_lis)/len(temp_lis)
-print(avg)
-        
+# temp_lis = data["temp"].to_list()
+# print(temp_lis)
+# avg = sum(temp_lis)/len(temp_lis)
+# # print(avg)
+# # print(data["temp"].max())
+# print(data["condition"])
+# print(data.condition)
+
+# get data in row
+# print(data[data.day == data.day.max()])
+
+# print(data[data.temp == data.temp.max()])
+
+
+# monday = data[data.day == "Monday"]
+# monday_temp = monday.temp[0]
+# monday_temp_F = monday_temp * 9 / 5 + 32
+# print(data.day, "temperature was",monday_temp_F)
+
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+
+# data = pandas.DataFrame(data_dict)
+# dataxls = data.to_csv("/Users/sudipto/Documents/code/projects/100days of code/day25 pandas/new_data.csv")
+
+
+# i have to analyse the csv file and findout the color, count and the total number of squrrels in the csv file
+
+
+# data = pandas.read_csv("/Users/sudipto/Documents/code/projects/100days of code/day25 pandas/2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20250119.csv")
+# gray_squrrels = len(data[data["Primary Fur Color"] == "Gray"])
+# cinnamon_squrrels = len(data[data["Primary Fur Color"] == "Cinnamon"])
+# black_squrrels = len(data[data["Primary Fur Color"] == "Black"])
+
+# # print(gray_squrrels)
+# # print(cinnamon_squrrels)
+# # print(black_squrrels)
+
+# data_dict = {
+#     "Fur Color": ["Gray", "Cinnamon", "Black"],
+#     "Count": [gray_squrrels, cinnamon_squrrels, black_squrrels]
+   
+# }
+# df = pandas.DataFrame(data_dict)
+# df.to_csv("/Users/sudipto/Documents/code/projects/100days of code/day25 pandas/squrrel_count.csv")      
